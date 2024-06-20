b03708e45cccc8bb3c06744cf05432591c68f559
Felipe <fefelbf@gmail.com>
2024-06-20 14:40:04 -0300

    comit

--- script/index.js
+++ script/index.js
@@ -29,3 +29,66 @@ hoverMoveInformations.addEventListener('mouseout', () =>{
     hoverMoves.style.scale = '1';
     hoverMoveInformations.style.display = 'none';
 });
+
+/*menu*/
+let menu =  document.querySelector('header');
+let notification = document.querySelector('.bi-bell');
+let notificationFill = document.querySelector('.bi-bell-fill');
+let arrowDown = document.querySelector('.bi-caret-down-fill');
+let arrowUp = document.querySelector('.bi-caret-up-fill');
+let imageLog = document.querySelector('.div-header-perfil img')
+
+const menuColor = setInterval(() =>{
+    if(window.scrollY > 10){
+        menu.style.background = '#000';
+    }else{
+        menu.style.background = 'transparent';
+    }
+}, 50);
+
+notification.addEventListener('click', () =>{
+    notification.style.display = 'none';
+    notificationFill.style.display = 'block';
+});
+
+notificationFill.addEventListener('click', () =>{
+    notification.style.display = 'block';
+    notificationFill.style.display = 'none';
+});
+
+const menuPerfil = setInterval(() =>{
+    arrowDown.addEventListener('click', () =>{
+        arrowDown.style.display = 'none';
+        arrowUp.style.display = 'block';
+    });
+    arrowUp.addEventListener('click', () =>{
+        arrowDown.style.display = 'block';
+        arrowUp.style.display = 'none';
+    });
+
+    imageLog.addEventListener('click', () =>{
+        arrowDown.style.display = 'none';
+        arrowUp.style.display = 'block';
+    });
+}, 50)
+
+//ratio
+let star = document.querySelector('.bi-star-fill');
+let starFill = document.querySelector('#special-star');
+
+const starRate = setInterval(() =>{
+star.addEventListener('click', () =>{
+    star.style.display = 'none';
+    starFill.style.display = 'inline';
+});
+},50);
+
+//seacrh
+let searchButton = document.querySelector('.bi-search');
+let searchDiv = document.querySelector('.div-search');
+
+const searchOpen = setInterval(() =>{
+    searchButton.addEventListener('click' () =>{
+        searchDiv.style.width = '500px';
+    })
+},50);
