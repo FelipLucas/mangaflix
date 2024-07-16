6a5d1625a381703f3ca3a7f73b4b1be9a5e76669
Felipe <fefelbf@gmail.com>
2024-07-16 01:19:36 -0300

    commit

--- script/animes.js
+++ script/animes.js
@@ -224,7 +224,9 @@ likeDownFill.addEventListener('click', () =>{
 });
 
 //funçções video
+let divVideo = document.querySelector('.div-player-video');
 let video = document.querySelector('.div-player-video video');
+let spacePlay = false;
 let controlsVideo = document.querySelector('.section-video-controls');
 
 let playVideo = document.querySelector(`.bi-play-fill-video`);
@@ -283,9 +285,11 @@ playVideo.addEventListener('click', () =>{
 
    setInterval(uptadeTImer, 100);
 
+   volume.addEventListener('click', mutedVol);
    document.addEventListener('keydown', pauseVideoKey);
-   setInterval(uptadeTImer, 100);
-
+   video.addEventListener('waiting', loader);
+   video.addEventListener('playing', loader);
+   
    setTimeout(() =>{
       controlsVideo.style.display = 'none';  
 
@@ -379,36 +383,98 @@ pauseVideoTwo.addEventListener('click', () =>{
 
    clearInterval(uptadeTImer, 100);
 
-   /*setTimeout(() =>{
-      pauseOverlay.style.display = 'flex';
-
-      setInterval(() =>{
-         video.addEventListener('click', () =>{
-            pauseOverlay.style.display = 'none';
-         });
-         playVideo.addEventListener('click', () =>{
-            pauseOverlay.style.display = 'none';
-         });
-      }, 50);
-   }, 30000);*/
-
    video.pause();
 });
 
+//pause with key
 function pauseVideoKey(event){
-   if(event.code === 'Space'){
+   if(event.code === 'Space' && spacePlay === false){
       video.pause();
+      spacePlay = true;
+
+      playVideoTwo.style.display = 'block';
+      pauseVideoTwo.style.display = 'none';
+
+      playVideo.style.display = 'block';
+      pauseVideo.style.display = 'none';
+
+
+   } else if(event.code === 'Space' && spacePlay === true){
+      video.play();
+      spacePlay = false;
+
+      playVideoTwo.style.display = 'none';
+      pauseVideoTwo.style.display = 'block';
+
+      playVideo.style.display = 'none';
+      pauseVideo.style.display = 'block';
 
       setTimeout(() =>{
-         if(event.code === 'Space'){
-            video.play();
-         }
-      }, 1000);
+         playVideo.style.display = 'none';
+         pauseVideo.style.display = 'none';
+   
+         setTimeout(() =>{
+            controlsVideo.style.display = 'none';  
+   
+            video.addEventListener('click', () =>{
+               playVideo.style.display = 'block';
+               pauseVideo.style.display = 'none';
+   
+               video.pause();
+   
+               setInterval(() =>{
+                  video.addEventListener('click', () =>{
+                     controlsVideo.style.display = 'block';  
+      
+                     playVideo.style.display = 'none';
+                     pauseVideo.style.display = 'block';
+      
+                     setInterval(() =>{
+                        playVideo.style.display = 'none';
+                     }, 2000)
+      
+                     video.play();
+                  });
+               }, 10);
+            });
+         },2000);
+      }, 7000)
    }
 };
-
 //aumentar o volume e aparecer para abaixar
-volume.addEventListener('click', () =>{
+volume.addEventListener('mousedown', startDrag);
+volume.addEventListener('mouseup', startDrag);
+volume.addEventListener('mousemove', showVolume);
+function startDrag(event){
+   if(event.type === 'mousedown'){
+      volumeDrag = true;
+   } else{
+      volumeDrag = false;
+   }
+}
+
+function showVolume(event){
+   if(volumeDrag){
+      var w = volume.clientWidth - 2;
+      var x = event.clientX - volume.offsetLeft;
+      var pctVol = x/w;
+
+      volumeRange.style.width = x + 'px';
+      video.volume = pctVol;
+
+      if(pctVol <= 0){
+         volume.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" width="35" height="35" viewBox="0 0 24 24" role="img" data-icon="VolumeOffStandard" aria-hidden="true"><path fill-rule="evenodd" clip-rule="evenodd" d="M11 4.00003C11 3.59557 10.7564 3.23093 10.3827 3.07615C10.009 2.92137 9.57889 3.00692 9.29289 3.29292L4.58579 8.00003H1C0.447715 8.00003 0 8.44774 0 9.00003V15C0 15.5523 0.447715 16 1 16H4.58579L9.29289 20.7071C9.57889 20.9931 10.009 21.0787 10.3827 20.9239C10.7564 20.7691 11 20.4045 11 20V4.00003ZM5.70711 9.70714L9 6.41424V17.5858L5.70711 14.2929L5.41421 14H5H2V10H5H5.41421L5.70711 9.70714ZM15.2929 9.70714L17.5858 12L15.2929 14.2929L16.7071 15.7071L19 13.4142L21.2929 15.7071L22.7071 14.2929L20.4142 12L22.7071 9.70714L21.2929 8.29292L19 10.5858L16.7071 8.29292L15.2929 9.70714Z" fill="currentColor"></path></svg>';
+      } else if(pctVol > 0 && pctVol <= .5){
+         volume.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" width="35" height="35" viewBox="0 0 24 24" role="img" data-icon="VolumeLowStandard" aria-hidden="true"><path fill-rule="evenodd" clip-rule="evenodd" d="M11 4.00003C11 3.59557 10.7564 3.23093 10.3827 3.07615C10.009 2.92137 9.57889 3.00692 9.29289 3.29292L4.58579 8.00003H1C0.447715 8.00003 0 8.44774 0 9.00003V15C0 15.5523 0.447715 16 1 16H4.58579L9.29289 20.7071C9.57889 20.9931 10.009 21.0787 10.3827 20.9239C10.7564 20.7691 11 20.4045 11 20V4.00003ZM5.70711 9.70714L9 6.41424V17.5858L5.70711 14.2929L5.41421 14H5H2V10H5H5.41421L5.70711 9.70714ZM16.0001 12C16.0001 10.4087 15.368 8.88262 14.2428 7.7574L12.8285 9.17161C13.5787 9.92176 14.0001 10.9392 14.0001 12C14.0001 13.0609 13.5787 14.0783 12.8285 14.8285L14.2428 16.2427C15.368 15.1175 16.0001 13.5913 16.0001 12Z" fill="currentColor"></path></svg>';
+      } else{
+         volume.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" width="35" height="35" viewBox="0 0 24 24" role="img" data-icon="VolumeMediumStandard" aria-hidden="true"><path fill-rule="evenodd" clip-rule="evenodd" d="M11 4.00003C11 3.59557 10.7564 3.23093 10.3827 3.07615C10.009 2.92137 9.57889 3.00692 9.29289 3.29292L4.58579 8.00003H1C0.447715 8.00003 0 8.44774 0 9.00003V15C0 15.5523 0.447715 16 1 16H4.58579L9.29289 20.7071C9.57889 20.9931 10.009 21.0787 10.3827 20.9239C10.7564 20.7691 11 20.4045 11 20V4.00003ZM5.70711 9.70714L9 6.41424V17.5858L5.70711 14.2929L5.41421 14H5H2V10H5H5.41421L5.70711 9.70714ZM17.0709 4.92897C18.9462 6.80433 19.9998 9.34787 19.9998 12C19.9998 14.6522 18.9462 17.1957 17.0709 19.0711L15.6567 17.6569C17.157 16.1566 17.9998 14.1218 17.9998 12C17.9998 9.87831 17.157 7.84347 15.6567 6.34318L17.0709 4.92897ZM14.2428 7.7574C15.368 8.88262 16.0001 10.4087 16.0001 12C16.0001 13.5913 15.368 15.1175 14.2428 16.2427L12.8285 14.8285C13.5787 14.0783 14.0001 13.0609 14.0001 12C14.0001 10.9392 13.5787 9.92176 12.8285 9.17161L14.2428 7.7574Z" fill="currentColor"></path></svg>';
+      }
+
+      volume.removeEventListener('click', mutedVol);
+   }
+}
+
+volume.addEventListener('mouseover', () =>{
    volumeRange.style.display = 'block';
 
    setInterval(() =>{
@@ -416,7 +482,23 @@ volume.addEventListener('click', () =>{
          volumeRange.style.display = 'none';
       });
    }, 50);
+
 });
+
+volume.addEventListener('mouseout', () =>{
+   volumeRange.style.display = 'none';
+   volume.addEventListener('click', mutedVol);
+});
+
+function mutedVol(){
+   if(!video.muted){
+      video.muted = true;
+      volume.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" width="35" height="35" viewBox="0 0 24 24" role="img" data-icon="VolumeOffStandard" aria-hidden="true"><path fill-rule="evenodd" clip-rule="evenodd" d="M11 4.00003C11 3.59557 10.7564 3.23093 10.3827 3.07615C10.009 2.92137 9.57889 3.00692 9.29289 3.29292L4.58579 8.00003H1C0.447715 8.00003 0 8.44774 0 9.00003V15C0 15.5523 0.447715 16 1 16H4.58579L9.29289 20.7071C9.57889 20.9931 10.009 21.0787 10.3827 20.9239C10.7564 20.7691 11 20.4045 11 20V4.00003ZM5.70711 9.70714L9 6.41424V17.5858L5.70711 14.2929L5.41421 14H5H2V10H5H5.41421L5.70711 9.70714ZM15.2929 9.70714L17.5858 12L15.2929 14.2929L16.7071 15.7071L19 13.4142L21.2929 15.7071L22.7071 14.2929L20.4142 12L22.7071 9.70714L21.2929 8.29292L19 10.5858L16.7071 8.29292L15.2929 9.70714Z" fill="currentColor"></path></svg>'
+   } else{
+      video.muted = false;
+      volume.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" fill="none" width="35" height="35" viewBox="0 0 24 24" role="img" data-icon="VolumeHighStandard" aria-hidden="true"><path fill-rule="evenodd" clip-rule="evenodd" d="M24 12C24 8.28693 22.525 4.72597 19.8995 2.10046L18.4853 3.51468C20.7357 5.76511 22 8.81736 22 12C22 15.1826 20.7357 18.2348 18.4853 20.4852L19.8995 21.8995C22.525 19.2739 24 15.713 24 12ZM11 3.99995C11 3.59549 10.7564 3.23085 10.3827 3.07607C10.009 2.92129 9.57889 3.00685 9.29289 3.29285L4.58579 7.99995H1C0.447715 7.99995 0 8.44767 0 8.99995V15C0 15.5522 0.447715 16 1 16H4.58579L9.29289 20.7071C9.57889 20.9931 10.009 21.0786 10.3827 20.9238C10.7564 20.7691 11 20.4044 11 20V3.99995ZM5.70711 9.70706L9 6.41417V17.5857L5.70711 14.2928L5.41421 14H5H2V9.99995H5H5.41421L5.70711 9.70706ZM16.0001 12C16.0001 10.4087 15.368 8.88254 14.2428 7.75732L12.8285 9.17154C13.5787 9.92168 14.0001 10.9391 14.0001 12C14.0001 13.0608 13.5787 14.0782 12.8285 14.8284L14.2428 16.2426C15.368 15.1174 16.0001 13.5913 16.0001 12ZM17.0709 4.92889C18.9462 6.80426 19.9998 9.3478 19.9998 12C19.9998 14.6521 18.9462 17.1957 17.0709 19.071L15.6567 17.6568C17.157 16.1565 17.9998 14.1217 17.9998 12C17.9998 9.87823 17.157 7.8434 15.6567 6.34311L17.0709 4.92889Z" fill="currentColor"></path></svg>'
+   }
+}
 
 //hover proximo episodio
 moreVideosButton.addEventListener('mouseover', () =>{
@@ -461,17 +543,22 @@ captionPortuguese.addEventListener('click', () =>{
    playVideo.style.display = 'block';
    pauseVideo.style.display = 'none';
 
-   video.src = 'https://rr8---sn-8p8v-bg0es.googlevideo.com/videoplayback?expire=1721033349&ei=BXKUZpbYF-WMxtYPra6ouAU&ip=179.193.11.19&id=9c76d2711981528f&itag=18&source=blogger&xpc=Egho7Zf3LnoBAQ%3D%3D&mh=iP&mm=31&mn=sn-8p8v-bg0es&ms=au&mv=m&mvi=8&pl=24&susc=bl&eaua=pPBo9J5J0jc&mime=video/mp4&vprv=1&dur=1569.134&lmt=1698372276845369&mt=1721004304&txp=1311224&sparams=expire,ei,ip,id,itag,source,xpc,susc,eaua,mime,vprv,dur,lmt&sig=AJfQdSswRgIhAPgr_OUn5nC4UBKI6tKSApEBOmhUa_sbB-Wffz9Pkd7SAiEAlPO1ZT-Bg54jxbXqG_HrULZFk84Gsv3qAsBSeHz6Q8k%3D&lsparams=mh,mm,mn,ms,mv,mvi,pl&lsig=AHlkHjAwRQIgUub6T4z4bBKy1KjwO6zBEoaxQAGCcgxz-WUYSHYZ4PUCIQCzgQ4K2micYl_TFwOwXPD91jFxoZ-bliVLd2kREujBaw%3D%3D&cpn=V4OSBjdKaCiDlcJq&c=WEB_EMBEDDED_PLAYER&cver=1.20240709.01.00';
+   video.src = 'https://rr8---sn-8p8v-bg0es.googlevideo.com/videoplayback?expire=1721124601&ei=edaVZvX6N7eN3rsPz7qfwAM&ip=179.193.11.19&id=9c76d2711981528f&itag=22&source=blogger&xpc=Egho7Zf3LnoBAQ%3D%3D&mh=iP&mm=31&mn=sn-8p8v-bg0es&ms=au&mv=m&mvi=8&pl=24&susc=bl&eaua=pPBo9J5J0jc&mime=video/mp4&vprv=1&dur=1569.134&lmt=1698372873389649&mt=1721095265&txp=1311224&sparams=expire,ei,ip,id,itag,source,xpc,susc,eaua,mime,vprv,dur,lmt&sig=AJfQdSswRAIgY9r3Y0u_6Pk3YrhclHHWnss_SShnhNDNwIJqLYKW-g4CIGLM1wyNYNj0EvMTXfBFbHRzGW_ewMxx1T5JRn7Si1Y8&lsparams=mh,mm,mn,ms,mv,mvi,pl&lsig=AHlkHjAwRgIhAMDp2y9APrEtsfb9OVWFg9gK2lVXSLl5_Fa7Jw2sOay1AiEAzzSfURqNkhqivbvk161HVOJl74_EehFkJz45ioE_ssc%3D&cpn=AlP760x_Fhsik5ZQ&c=WEB_EMBEDDED_PLAYER&cver=1.20240710.01.00';
 });
 
 //tela cheia
 fullScreenButton.addEventListener('click', () =>{
-   if(video.requestFullscreen){
-      video.requestFullscreen();
-   } else if(video.webKitFullScreen){
-      video.webKitFullScreen();
+   if(!document.webkitFullScreenElement){
+      divVideo.webkitRequestFullScreen();
+   } else{
+      document.webkitExitFullScreen();
    }
 });
+
+let iframe = document.querySelector('.iframe-watch');
+function loadIframe(){
+   iframe.play();
+}
 
 //skip video
 skipButton.addEventListener('click', () =>{
@@ -504,7 +591,7 @@ function convertTimer(hours, minutes, seconds){
 
    if(minutes < 10){
       minutes = '0' + String(minutes);
-   } else if(minutes > 50){
+   } else if(minutes > 59){
       minutes = minutes - (Math.floor(minutes / 60) * 60);
    }
 
@@ -519,12 +606,12 @@ function uptadeTImer(){
    seg = Math.floor((video.duration / 60) % 1) * 60;
 
    //current timer
-   currentHour = Math.florr(video.currentTime / 3600);
-   currentMin = Math.florr(video.currentTime / 60);
-   currentMin = Math.florr((video.currentTime / 60) % 1) * 60;
+   currentHour = Math.floor(video.currentTime / 3600);
+   currentMin = Math.floor(video.currentTime / 60);
+   currentSeg = Math.floor(((video.currentTime / 60) % 1) * 60);
 
    timerVideo.innerHTML = convertTimer(currentHour, currentMin, currentSeg);
-
+   
    bufferedEnd = video.buffered.end(video.buffered.length - 1);
    videoLoader.style.width = String((bufferedEnd / video.duration) * 100) + '%';
    pctSeek = (video.currentTime / video.duration) * 100;
@@ -536,6 +623,34 @@ function uptadeTImer(){
       setTimeout(() =>{
          classificacaoVideo.style.display = 'none';
       }, 5000);
+   }
+}
+
+//container pause
+function informationsPause(){
+   if(pauseVideoTwo.length = 0){
+      setTimeout(() =>{
+         pauseOverlay.style.display = 'flex';
+         overlayPause.style.display = 'block';
+      }, 5000);
+   } else{
+      pauseOverlay.style.display = 'none';
+      overlayPause.style.display = 'none';
+   }
+} setInterval(informationsPause, 50);
+//loader
+function loader(event){
+   switch(event.type){
+      case 'waiting':
+      videoLoader.style.display = 'block';
+      break;
+
+      case 'playing':
+      videoLoader.style.display = 'none';
+      break;
+
+      default:
+         videoLoader.style.display = 'none';
    }
 }
 
@@ -554,28 +669,6 @@ barProgess.addEventListener('click', seeker)
 function seeker(event){
    pctSeekBar = (event.clientX / barProgess.clientWidth) * 100;
    video.currentTime = (video.duration * pctSeekBar) / 100;
-}
-
-volume.addEventListener('mousedown', startDrag);
-volume.addEventListener('mouseup', startDrag);
-volume.addEventListener('mousemove', showVolume);
-function startDrag(event){
-   if(event.type === 'mousedown'){
-      volumeDrag = true;
-   } else{
-      volumeDrag = false;
-   }
-}
-
-function showVolume(event){
-   if(volumeDrag){
-      var w = volume.clientWidth - 2;
-      var x = event.clientX - volume.offsetLeft;
-      var pctVol = x/w;
-
-      volumeRange.style.width = x + 'px';
-      video.volume = pctVol;
-   }
 }
 
 //movies hover
