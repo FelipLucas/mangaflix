d85daf07b09dcf54ebe87769ed87efb8c9e616d7
Felipe <fefelbf@gmail.com>
2024-07-20 23:00:08 -0300

    commit

--- script/animes.js
+++ script/animes.js
@@ -986,20 +986,12 @@ shareButtonLeft.addEventListener('click', () =>{
 //copy text
 urlButton.addEventListener('click', () =>{
    urlButton.innerHTML = 'Copied' +  '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16"><path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/></svg>';
+   navigator.clipboard.writeText(currentUrl);
 
    setTimeout(() =>{
       urlButton.innerHTML = 'Copy';
    }, 1000);
-})
-
-function copyText(){
-   textUrl.select();
-   textUrl.setSelectionRange(0, 99999);
-
-   document.execCommand("copy");
-   window.alert('hello world');
-}
-
+});
 
 //movies hover
 let imageMovieHover = document.querySelector(".div-movie-slide img");
